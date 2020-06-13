f04ee5faa8a38edc3a3a25e5c6df91ec1be40fb4
anies <arocnies@gmail.com>
2020-06-13 13:02:16 -0400

    Added star field background

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,7 +1,26 @@
 import com.soywiz.korge.Korge
+import com.soywiz.korge.view.*
 import com.soywiz.korim.color.Colors
+import kotlin.random.Random
 
-suspend fun main() = Korge(width = 1600, height = 900, bgcolor = Colors["#031e4a"]) {
+suspend fun main() = Korge(width = 1600, height = 900, bgcolor = Colors["#010a19"]) {
+    setupStarField(SolidRect(width = 1.0, height = 1.0, color = Colors.WHITE), 0.01)
+
     val game = Game(this)
     game.start()
+}
+
+/**
+ * [density] value of 1.0 means a star per pixel randomly placed in the container.
+ */
+private fun Container.setupStarField(star: View, density: Double) {
+    val numberOfStars = (this.width * this.height * density).toInt()
+    repeat(numberOfStars) {
+        this.addChild(
+                star.clone().position(
+                        Random.nextInt((width + 1).toInt()),
+                        Random.nextInt((height + 1).toInt())
+                ).alpha(Random.nextDouble())
+        )
+    }
 }
